296103854e9b0d5aca9255afc3005991149a6f33
Mustafa Gözcü <gozcumustafa34@gmail.com>
2023-10-16 11:16:40 +0200

    added strings methods

---
+++ Strings.js
@@ -0,0 +1,44 @@
+let learnJs = "How to Learn JavaScript Language.";
+
+let sonuc;
+
+sonuc = learnJs.toLowerCase();
+console.log(sonuc);
+sonuc = learnJs.toUpperCase();
+console.log(sonuc);
+sonuc = learnJs.length;
+console.log(sonuc);
+sonuc = learnJs[1];
+console.log(sonuc);
+sonuc = learnJs.slice(0, 6);
+console.log(sonuc);
+sonuc = learnJs.slice(10);
+console.log(sonuc);
+sonuc = learnJs.slice(-10);
+console.log(sonuc);
+sonuc = learnJs.slice(-10, -5);
+console.log(sonuc);
+
+sonuc = learnJs.substring(0, 6);
+console.log(sonuc);
+sonuc = learnJs.substring(10);
+console.log(sonuc);
+
+sonuc = learnJs.replace("Learn","Dont Learn");
+console.log(sonuc);
+sonuc = learnJs.trim();
+console.log(sonuc);
+sonuc = learnJs.trimEnd();
+console.log(sonuc);
+sonuc = learnJs.trimStart();
+console.log(sonuc);
+
+sonuc = learnJs.indexOf("Komple");
+console.log(sonuc);
+sonuc = learnJs.split(" ");
+console.log(sonuc);
+sonuc = learnJs.split(" ")[0];
+console.log(sonuc);
+sonuc = learnJs.split(" ")[3];
+console.log(sonuc);
+

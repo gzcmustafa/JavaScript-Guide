48437a97e5f175b2f86cfce081fa315d187627a7
Mustafa Gözcü <gozcumustafa34@gmail.com>
2023-11-14 17:30:52 +0100

    added ErrorHandling and staticMethod

---
+++ ErrorHandling.js
@@ -0,0 +1,31 @@
+
+let fullName
+
+try{
+    let firstName = 'Mustafa'
+    let lastName = 'Yunus'
+    fullName = firstName + ' ' + surname
+    
+
+} catch (e) {
+    console.log(e)
+}
+console.log(fullName)
+
+// ERROR TYPES
+
+//ReferanceError:
+let firstName = "Kerem"
+let fullName = firstName + ' ' + lastName
+console.log(fullName);
+
+//syntaxError:
+let karesiniAL = 2 x 2
+console.log(karesiniAL)
+
+//TypeError:
+let num = 10
+console.log(num.toLowerCase())
+
+
+

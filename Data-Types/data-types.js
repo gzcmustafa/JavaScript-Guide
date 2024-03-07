3cff0ca47c3dde971dc340061641ac5a10d7d12b
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-03-07 23:39:37 +0300

    added readme file data-types

--- Data-Types/data-types.js
+++ Data-Types/data-types.js
@@ -1,74 +1,76 @@
-// Örnek-1
-let numberOne = 5
-let numberTwo = 5
-console.log(numberOne == numberTwo) // True
+// // Örnek-1
+// let numberOne = 5
+// let numberTwo = 5
+// console.log(numberOne == numberTwo) // True
 
-// Örnek-2
-let name = 'Mustafa'
-let city = 'Heidelberg'
-console.log(name == city) // False
-
-
-
-// Örnek-1
-let numbersA = [1,2,3]
-let numbersB = [1,2,3]
-console.log(numbersA == numbersB) // false
-
-// Örnek-2
-let numbersA = [1,2,3]
-let numbersB = numbersA
-console.log(numbersA == numbersB) // true
+// // Örnek-2
+// let name = 'Mustafa'
+// let city = 'Heidelberg'
+// console.log(name == city) // False
 
 
-const PI = 3.14
-let weight = 80
-let age = 25
+
+// // Örnek-1
+// let numbersA = [1,2,3]
+// let numbersB = [1,2,3]
+// console.log(numbersA == numbersB) // false
+
+// // Örnek-2
+// let numbersA = [1,2,3]
+// let numbersB = numbersA
+// console.log(numbersA == numbersB) // true
 
 
-let person = {
-    name:"Mustafa",
-    surname:"Gözcü",
-    city:"Heidelberg" 
-    };
+// const PI = 3.14
+// let weight = 80
+// let age = 25
 
 
-let car = { 
-        brand:"Audi",
-        model:"RS6",
-        engine: function(){
-             console.log("engine started!")}
-        }
+// let person = {
+//     name:"Mustafa",
+//     surname:"Gözcü",
+//     city:"Heidelberg" 
+//     };
+
+
+// let car = { 
+//         brand:"Audi",
+//         model:"RS6",
+//         engine: function(){
+//              console.log("engine started!")}
+//         }
         
-        car.engine();
+//         car.engine();
 
 
-        const PI = Math.PI  
-        console.log(PI) // 3.14 
+//         const PI = Math.PI  
+//         console.log(PI) // 3.14 
         
-        console.log(Math.round(9.81)); // Math.round metodunu kullanarak 
-                                       // 9.81 sayısını en yakın tam 
-                                       // sayıya yuvarlayabiliriz.
+//         console.log(Math.round(9.81)); // Math.round metodunu kullanarak 
+//                                        // 9.81 sayısını en yakın tam 
+//                                        // sayıya yuvarlayabiliriz.
         
-        let randomNum = Math.random(); // 0 ile 1 arasında (1 dahil değil) 
-                                       // rastgele bir ondalık sayı üretir.
-        console.log(randomNum);
+//         let randomNum = Math.random(); // 0 ile 1 arasında (1 dahil değil) 
+//                                        // rastgele bir ondalık sayı üretir.
+//         console.log(randomNum);
         
-        console.log(Math.sqrt(2)); // Math.sqrt metodunu kullanarak bir sayının karekökünü hesaplayabiliriz.
+//         console.log(Math.sqrt(2)); // Math.sqrt metodunu kullanarak bir sayının karekökünü hesaplayabiliriz.
 
-        l
-
-
-let language: "Javascript";
-let job:'Software Engineer';
-let details: "Hi everybody this article about Javascript."
+//         l
 
 
+// let language: "Javascript";
+// let job:'Software Engineer';
+// let details: "Hi everybody this article about Javascript."
 
 
 
-let name = "Mustafa"
-let surname = "Gözcü"
 
-let fullName =  name + surname;
-console.log(fullName)
+
+// let name = "Mustafa"
+// let surname = "Gözcü"
+
+// let fullName =  name + surname;
+// console.log(fullName)
+let string = 'JavaScript'
+console.log(string.substr(4,6)) // Script

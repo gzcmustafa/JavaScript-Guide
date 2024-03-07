5d94bf25ab2245359ef3913a44b1580af58e0508
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-03-07 18:55:25 +0300

    added data-types

---
+++ Data-Types/data-types.js
@@ -0,0 +1,74 @@
+// Örnek-1
+let numberOne = 5
+let numberTwo = 5
+console.log(numberOne == numberTwo) // True
+
+// Örnek-2
+let name = 'Mustafa'
+let city = 'Heidelberg'
+console.log(name == city) // False
+
+
+
+// Örnek-1
+let numbersA = [1,2,3]
+let numbersB = [1,2,3]
+console.log(numbersA == numbersB) // false
+
+// Örnek-2
+let numbersA = [1,2,3]
+let numbersB = numbersA
+console.log(numbersA == numbersB) // true
+
+
+const PI = 3.14
+let weight = 80
+let age = 25
+
+
+let person = {
+    name:"Mustafa",
+    surname:"Gözcü",
+    city:"Heidelberg" 
+    };
+
+
+let car = { 
+        brand:"Audi",
+        model:"RS6",
+        engine: function(){
+             console.log("engine started!")}
+        }
+        
+        car.engine();
+
+
+        const PI = Math.PI  
+        console.log(PI) // 3.14 
+        
+        console.log(Math.round(9.81)); // Math.round metodunu kullanarak 
+                                       // 9.81 sayısını en yakın tam 
+                                       // sayıya yuvarlayabiliriz.
+        
+        let randomNum = Math.random(); // 0 ile 1 arasında (1 dahil değil) 
+                                       // rastgele bir ondalık sayı üretir.
+        console.log(randomNum);
+        
+        console.log(Math.sqrt(2)); // Math.sqrt metodunu kullanarak bir sayının karekökünü hesaplayabiliriz.
+
+        l
+
+
+let language: "Javascript";
+let job:'Software Engineer';
+let details: "Hi everybody this article about Javascript."
+
+
+
+
+
+let name = "Mustafa"
+let surname = "Gözcü"
+
+let fullName =  name + surname;
+console.log(fullName)

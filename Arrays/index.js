214c03727f0080ddb83e44026b3e4e5e3198da99
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-03-17 13:37:18 +0300

    added arrays

---
+++ Arrays/index.js
@@ -0,0 +1,59 @@
+// const arr = Array()
+// console.log(arr)
+
+// const arr = []
+// console.log(arr)
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.length);
+
+// const str = "Apple,Banana,Orange";
+// const fruits = str.split(","); // "," ayırıcıya göre dizeyi böler
+
+// console.log(fruits); // ["Apple", "Banana", "Orange"]
+
+// const arr = Array(8).fill("X")
+// console.log(arr);
+
+// const arr1= [1,2,3];
+// const arr2 = ["a",'b','c']
+// const newArr = arr2.concat(arr1)
+// console.log(newArr)
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Apple', 'Mango'];
+// console.log(fruits.indexOf('Apple'))
+// console.log(fruits.indexOf('Apple',2))
+// console.log(fruits.indexOf('dsadsa'))
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Apple', 'Mango'];
+// console.log(fruits.lastIndexOf('Apple'))
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+// console.log(fruits.includes('Apple')); 
+// console.log(Array.isArray(fruits))
+
+// const numbers = [1,2,3,4,5,6]
+// console.log(numbers.toString())
+
+// const fruits = ['Apple','Banana','Orange']
+// console.log(fruits.join(' # ---'))
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Kiwi'];
+// console.log(fruits.slice(1, 3));
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+
+// fruits.splice(2,1);
+// console.log(fruits)
+// fruits.splice(2, 0, 'Grapes', 'Kiwi');
+// console.log(fruits)
+
+// const fruits = ['Apple', 'Banana', 'Orange'];
+// fruits.push('Mango',2,2321321);
+// console.log(fruits);
+
+// const fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+
+// const removedItem = fruits.pop();
+// console.log(removedItem); // 'Mango'
+// console.log(fruits); 

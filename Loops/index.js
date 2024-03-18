5dc93e53cf4017eca6ce6905702a30dcb65da782
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-03-18 16:05:15 +0300

    added loops folder

---
+++ Loops/index.js
@@ -0,0 +1,21 @@
+// let i = 0;
+
+// while (i<=5) {
+    
+//     i++
+// }
+// console.log(`i'nin değeri ${i}`);
+
+
+// let i = 0
+// do {
+//     i++
+// } while (i<=5)
+
+// console.log(i)
+
+// const arr = [1,2,3,4,5]
+
+// for (const element of arr){
+//     console.log(element);
+// }

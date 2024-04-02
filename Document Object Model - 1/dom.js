3e95edff6a62fd82f0595fb0c61d693c61966fbd
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-04-02 23:42:30 +0300

    modified dom.js

--- Document Object Model - 1/dom.js
+++ Document Object Model - 1/dom.js
@@ -13,10 +13,26 @@
 
 // const h1 = document.getElementsByClassName('title-1')
 // console.log(h1)
-document.querySelectorAll('h1').forEach(h1,index => {
-    if(index % 2 == 0) {
-        h1.style.color='green'
-    } else {
-        h1.style.color = 'yellow'
-    }
-});
+// document.querySelectorAll('h1').forEach(h1,index => {
+//     if(index % 2 == 0) {
+//         h1.style.color='green'
+//     } else {
+//         h1.style.color = 'yellow'
+//     }
+// });
+
+
+
+for (let i = 0; i < 10; i++) {
+    let title =  document.createElement('h1')
+    title.className = 'title'
+    title.style.fontSize = '24px'
+    title.textContent = 'dsadsada'
+    title.style.backgroundColor = 'red'
+
+document.body.append(title)
+    
+}
+
+  
+    
